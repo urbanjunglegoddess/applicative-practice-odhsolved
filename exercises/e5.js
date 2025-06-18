5c43a9dc4c294b50404ed91b901df817db087305
Omegea Hunter <omegeadhunter@gmail.com>
2025-06-18 14:03:26 -0400

    add function to retrieve planet names with mass value above a given number

--- exercises/e5.js
+++ exercises/e5.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithMassValue(data, number) {
-  // Your code goes here...
+  const planetsWithMass = data.planets;
+  return planetsWithMass.filter (planet => planet.mass.massValue >= number).map(planet => planet.name);;
 }
 
 
